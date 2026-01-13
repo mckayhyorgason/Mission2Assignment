3c1a14f51778dc60a5439961aeb6b88e54620a9b
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-01-12 21:43:32 -0700

    added comments

--- DiceRoll.cs
+++ DiceRoll.cs
@@ -2,22 +2,27 @@ namespace DiceRolling;
 
 public class DiceRoll
 {
-    
+    //Method for rolling the dice
     public int[] Roll(int numRoll)
     {
+        //importing random library
         Random rand = new Random();
+        //setting the array for dice totals
         int[] diceTotals = new int[13];
 
+        //loop to roll the dice the amount given by user
         for (int i = 0; i < numRoll; i++)
         {
             int die1 = rand.Next(1, 7);
             int die2 = rand.Next(1, 7);
 
+            //both die added together
             int rollTotal = die1 + die2;
             
+            //one is added to the spot in the array equal to the dice total
             diceTotals[rollTotal]++;
         }
-        
+        //returns the full array
         return (diceTotals);
     }
     
@@ -25,11 +30,14 @@ public class DiceRoll
     {
         double[] dicePercentage = new double[13];
         
+        //loop that calculates the percentage of each number rolled and prints an * for every percent
         for (int i = 2; i < 13; i++)
         {
             System.Console.WriteLine();
             dicePercentage[i] = ((double)diceTotals[i] / numRoll) * 100;
+            //prints the number spot in the array
             System.Console.Write($"{i}: ");
+            //prints an * for every percent
             for (int j = 0; j < dicePercentage[i]; j++) 
             {
                 System.Console.Write($"*");
--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Mckay Yorgason
+
+// See https://aka.ms/new-console-template for more information
 using DiceRolling;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        //instance of DiceRoll
         DiceRoll dr = new DiceRoll();
         
+        //set roll count to zero
         int roll_count = 0;
         
+        
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        //ask user how many times to roll the dice
         System.Console.WriteLine("How many times do you want to roll the dice?");
         roll_count = int.Parse(System.Console.ReadLine());
         
@@ -17,6 +23,7 @@ public class Program
         System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         System.Console.WriteLine($"Total number of rolls = {roll_count}.");
         
+        //results from the dice that were rolled
         int[] results = dr.Roll(roll_count);
 
         dr.Calculate(roll_count, results);
